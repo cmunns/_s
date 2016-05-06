a7566edb8dd8f9dcec56827eeac4fb51dbb53de1
Adam Munns <adam@whetinteractive.com>
2016-05-06 07:40:38 -0700

    go back to annoying delegate event submit on gf bc the button doesn't except same hover as a.x-btn

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,8 @@
 (function($){
 
-    // $('.gform_footer').on('click', 'a.x-btn', function(e){
-    //     e.preventDefault();
-    //     $(e.delegateTarget).find('[type="submit"]').trigger('click');
-    // });
+    $('.gform_footer').on('click', 'a.x-btn', function(e){
+        e.preventDefault();
+        $(e.delegateTarget).find('[type="submit"]').trigger('click');
+    });
 
 })(jQuery);
--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -2,11 +2,11 @@
 function gf_make_submit_input_into_a_button_element($button_input, $form) {
 
 	//save attribute string to $button_match[1]
-	preg_match("/<input([^\/>]*)(\s\/)*>/", $button_input, $button_match);
-	$button_atts = str_replace("value='".$form['button']['text']."' ", "", $button_match[1]);
-	$button_atts = str_replace( "gform_button button", "x-btn x-btn-global", $button_atts );
+	// preg_match("/<input([^\/>]*)(\s\/)*>/", $button_input, $button_match);
+	// $button_atts = str_replace("value='".$form['button']['text']."' ", "", $button_match[1]);
+	// $button_atts = str_replace( "gform_button button", "x-btn x-btn-global", $button_atts );
 
-	return '<button '.$button_atts.'>'.$form['button']['text'].'</button>';
+	return '<div class="hidden">'.$button_input.'</div><a href="#" class="trigger-submit x-btn x-btn-global">'.$form['button']['text'].'</a>';
 	// $atts = array(
 	// 	'href' => null,
 	// 	'content' => $form['button']['text'],
